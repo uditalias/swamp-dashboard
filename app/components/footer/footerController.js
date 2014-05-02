640779f129ef66bf30cdc640dfbdf1d5c723db3c
udidu <udi.talias@gmail.com>
2014-05-02 16:44:41 +0300

    added the swamp version to footer title

--- app/components/footer/footerController.js
+++ app/components/footer/footerController.js
@@ -8,7 +8,8 @@ angular.module('swamp.controllers').controller('footerController', ['$scope', '$
             initializing: true,
             title: '',
             maximized: false,
-            panelContentHeight: 300
+            panelContentHeight: 300,
+            swampVersion: window.swampVersion
         };
         $scope.tabsContent = [];
 
