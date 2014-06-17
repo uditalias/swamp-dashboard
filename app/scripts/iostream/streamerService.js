24268cfe555ee5c27a65e632fbfd59463f004989
udidu <udi.talias@gmail.com>
2014-06-17 19:42:08 +0300

    io streamer

--- app/scripts/iostream/streamerService.js
+++ app/scripts/iostream/streamerService.js
@@ -14,20 +14,33 @@ var streamerService = (function() {
     Streamer.prototype._createXhrObject = function() {
         this._xhr = new XMLHttpRequest();
 
+        this._xhr.onload = this._onLoad.bind(this);
+        this._xhr.onloadend = this._onLoadEnd.bind(this);
         this._xhr.onreadystatechange = this._onXhrStateChanged.bind(this);
     };
 
     Streamer.prototype.poll = function() {
+
+        $('.io-container pre').text('');
 
         this._xhr.open('GET' ,this._getStreamUri() ,true);
 
         this._xhr.send(null);
     };
 
+    Streamer.prototype._onLoad = function() {
+        $('.io-container pre').text(this._xhr.responseText);
+    };
+
+    Streamer.prototype._onLoadEnd = function() {
+    };
+
     Streamer.prototype._onXhrStateChanged = function() {
 
-        console.log(this._xhr.response);
+        if(this._xhr.readyState == 4) {
 
+
+        }
     };
 
     Streamer.prototype._getStreamUri = function() {
