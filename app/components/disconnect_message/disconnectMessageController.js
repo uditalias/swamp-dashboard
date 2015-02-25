604fd04759a89184a6efa74afaf582aff255f7f6
udidu <udi.talias@gmail.com>
2015-02-25 11:35:09 +0200

    added messages

--- app/components/disconnect_message/disconnectMessageController.js
+++ app/components/disconnect_message/disconnectMessageController.js
@@ -1,35 +1,46 @@
 'use strict';
-angular.module('swamp.controllers').controller('disconnectMessageController', ['$scope', 'env', '$timeout', function($scope, env, $timeout) {
+angular.module('swamp.controllers').controller('disconnectMessageController', [
+
+  '$scope', 'env', '$timeout', 'speechService',
+
+  function ($scope, env, $timeout, speechService) {
 
     $scope.handler = {
-        reconnectTimeout: env.swampReloadSeconds
-    }
+      reconnectTimeout: env.swampReloadSeconds
+    };
 
     function _countDown() {
 
-        $scope.handler.reconnectTimeout--;
+      $scope.handler.reconnectTimeout--;
 
-        if($scope.handler.reconnectTimeout == 0) {
-
-            _reconnect();
-
-        } else {
-
-            $timeout(_countDown, 1000);
-
-        }
-    }
-
-    function _reconnect() {
-        window.location.reload();
-    }
-
-    $scope.reconnect = function() {
+      if ($scope.handler.reconnectTimeout == 0) {
 
         _reconnect();
 
+      } else {
+
+        speechService.speek($scope.handler.reconnectTimeout, true);
+
+        $timeout(_countDown, 1000);
+
+      }
     }
+
+    function _reconnect() {
+      window.location.reload();
+    }
+
+    $scope.reconnect = function () {
+
+      speechService.stop();
+
+      _reconnect();
+
+    };
+
+    speechService.speek('Houston, we have a problem', true);
+    speechService.speek('Reconnecting in', true);
 
     _countDown();
 
-}]);
+  }]);
